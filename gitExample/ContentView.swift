9cfc2dd64544fd9a2acb0b5004511a1f810395f3
ryan rossman <ryrossmagic@gmail.com>
2021-03-11 10:59:15 -0500

    Initial Commit

---
+++ gitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitExample
+//
+//  Created by Ryan Rossman (student LM) on 3/11/21.
+//  Copyright © 2021 Ryan Rossman (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitExampleTests/gitExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  gitExampleTests.swift
+//  gitExampleTests
+//
+//  Created by Ryan Rossman (student LM) on 3/11/21.
+//  Copyright © 2021 Ryan Rossman (student LM). All rights reserved.
+//
+
+import XCTest
+@testable import gitExample
+
+class gitExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
